dd8ba59d729f6fa27a03e0c2e13bb35bb48cdc7b
Summer Rizzo <srainrizzo@gmail.com>
2019-12-06 21:49:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def directors_totals(nds)
   dir_index = 0
 
   while dir_index < nds.length do
-    result[nds[dir_index][:name]] = gross_for_director(nds, dir_index)
+    result[nds[dir_index][:name]] = gross_for_director(nds[dir_index])
     dir_index += 1
   end
   result
@@ -19,7 +19,7 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(nds, dir_index)
+def gross_for_director(director_hash)
   mov_index = 0
   total_profit = 0
   while mov_index < nds[dir_index][:movies].count do
