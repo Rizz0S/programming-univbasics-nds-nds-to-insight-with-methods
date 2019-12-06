5784dedeb76a0ed70c0cc008bcd62cf96f7c3760
Summer Rizzo <srainrizzo@gmail.com>
2019-12-06 21:34:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,14 +18,12 @@ end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(nds)
-  dir_index = 0
+def gross_for_director(nds, dir_index)
   mov_index = 0
   total_profit = 0
   while mov_index < nds[dir_index][:movies].count do
     total_profit += nds[dir_index][:movies][mov_index][:worldwide_gross]
     mov_index += 1
   end
-  dir_index += 1
   total_profit
 end
