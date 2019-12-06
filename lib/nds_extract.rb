c2f67b966d045d118606c30b7656e123e28bfde0
Summer Rizzo <srainrizzo@gmail.com>
2019-12-06 21:37:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,6 +10,7 @@ def directors_totals(nds)
   dir_index = 0
 
   while dir_index < nds.length do
+    puts dir_index
     result[nds[dir_index][:name]] = gross_for_director(nds, dir_index)
     dir_index += 1
   end
